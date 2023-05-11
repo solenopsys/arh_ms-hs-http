4525b0fe152276d81f4578accd08774644db52d4
AlexStorm <info@alexstorm.ru>
2023-05-11 13:00:14 +0530

    buildx was be changed to nerdctl

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 build_push(){
-  docker buildx build  --platform ${ARCHS} -t ${REGISTRY}/${NAME}:latest  --output=type=registry,registry.insecure=true --push .
+  nerdctl build --platform=${ARCHS} --output type=image,name=${REGISTRY}/${NAME}:latest,push=true .
 }
 
 
